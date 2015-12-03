94832fec38844316c4ce49d4987a003102edf220
Vlad Vinogradov <vladt9@yandex.ru>
2015-12-03 14:20:12 +0300

    Updated Client and Server interfaces

--- Core/src/Interfaces/IServer.java
+++ Core/src/Interfaces/IServer.java
@@ -4,15 +4,16 @@ import DataStructures.BoardChange;
 import DataStructures.Room;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.UUID;
 
 public interface IServer extends Remote {
-    void register(String name);
-    void joinRoom(UUID roomId, UUID playerId);
-    void createRoom(String roomName, int size, UUID playerId);
-    void leaveRoom(UUID roomId, UUID playerId);
-    List<Room> getRooms();
-    void takeEdge(BoardChange boardChange, UUID roomId);
-    void error(String errorMessage);
+    void register(String name) throws RemoteException;
+    void joinRoom(UUID roomId, UUID playerId) throws RemoteException;
+    void createRoom(String roomName, int size, UUID playerId) throws RemoteException;
+    void leaveRoom(UUID roomId, UUID playerId) throws RemoteException;
+    List<Room> getRooms() throws RemoteException;
+    void takeEdge(BoardChange boardChange, UUID roomId) throws RemoteException;
+    void error(String errorMessage) throws RemoteException;
 }
